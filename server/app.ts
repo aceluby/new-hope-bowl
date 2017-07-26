2cc3d6f0ead1cd5b3bc18c58c4fbb406a7dd353e
Jake.Luby <Jake.Luby@target.com>
2017-07-25 19:29:12 -0500

    adding host

--- server/app.ts
+++ server/app.ts
@@ -13,6 +13,7 @@ import bodyParser = require('body-parser');
  * Get port from environment and store in Express.
  */
 const port = normalizePort(process.env.PORT || serverPort);
+const host = process.env.HOST || '0.0.0.0';
 app.set('port', port);
 
 
@@ -25,7 +26,7 @@ const server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port);
+server.listen(port, host);
 server.on('error', onError);
 server.on('listening', onListening);
 
